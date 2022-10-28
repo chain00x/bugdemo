ba1c4866e6e515479256bed144299932fab24eec
chain00x <90015694+chain00x@users.noreply.github.com>
2022-10-29 03:41:34 +0800

    Update symlink.php

--- www/symlink.php
+++ www/symlink.php
@@ -5,14 +5,15 @@ if ($_FILES["file"]["error"] > 0)
 }
 else
 {
-    $zip_path="/usr/share/nginx/html/uploads/".md5($_FILES["file"]["name"]);
+    $zip_path="/usr/share/nginx/html/uploads/".md5($_FILES["file"]["name"].time());
     move_uploaded_file($_FILES["file"]["tmp_name"],$zip_path);
     echo "文件url：/uploads/".md5($_FILES["file"]["name"])."<br>";
-    $zipf_path=md5($_FILES["file"]["name"])."_1";
+    $zipf_path="uploads/".md5($_FILES["file"]["name"].time())."_1";
+    echo $zipf_path."<br>";
     if (!file_exists($zipf_path)){
-        mkdir("/uploads/".$zipf_path);
+        mkdir($zipf_path);
     }
-    $path="/usr/share/nginx/html/uploads/".$zipf_path;
+    $path="/usr/share/nginx/html/".$zipf_path;
     exec('unzip '.$zip_path. ' -d '.$path);
     passthru("cat $path/* 2>&1");
 }
